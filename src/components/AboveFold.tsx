d50ee4c32ac00747230b7877c9633a653292bc1c
michael86 <michael8t6@gmail.com>
2024-01-03 20:12:12 +0000

    slider animation loop

--- src/components/AboveFold.tsx
+++ src/components/AboveFold.tsx
@@ -1,8 +1,9 @@
 import { ReactElement, useLayoutEffect, useRef, useState } from "react";
 import "./styles/above_fold.css";
-import gsap from "gsap";
+
 import { useGSAP } from "@gsap/react";
-import { showHeroSlider, showContactSlide, showServiceSlide } from "./utils/animations";
+import { showContact, showServices } from "./utils/animations";
+import { gsap } from "gsap/gsap-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPepperHot,
@@ -15,53 +16,49 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const AboveFold: React.FC = (): ReactElement => {
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [activeSlide, setActiveSlide] = useState<0 | 1>(0);
-  const showSliderTl = useRef<GSAPTimeline | null>(null);
-  const contactSlideTl = useRef<GSAPTimeline | null>(null);
-  const serviceSlideTl = useRef<GSAPTimeline | null>(null);
+  const contactContainer = useRef<HTMLDivElement | null>(null);
+  const serviceContainer = useRef<HTMLDivElement | null>(null);
+  const buttonsContainer = useRef<HTMLDivElement | null>(null);
+  const showContactTL = useRef<GSAPTimeline | undefined>();
+  const showServiceTL = useRef<GSAPTimeline | undefined>();
+  let activeSlide: 0 | 1 = 0;
 
-  useGSAP(
-    () => {
-      if (!ref.current) return;
-      const slidesContainer = ref.current.children[0];
-      const btnContainer = ref.current.children[1];
-      const images = slidesContainer.children;
-      const contactContainer = images[0];
-      const servicesContainer = images[1];
+  useGSAP(async () => {
+    if (
+      !serviceContainer.current ||
+      !contactContainer.current ||
+      !buttonsContainer.current
+    )
+      return;
 
-      showSliderTl.current = showHeroSlider(contactContainer, Array.from(btnContainer.children));
+    const buttons = Array.from(buttonsContainer.current.children);
 
-      gsap.set(servicesContainer, { display: "none" });
-      contactSlideTl.current = showContactSlide(contactContainer);
-      serviceSlideTl.current = showServiceSlide(servicesContainer);
+    showContactTL.current = showContact(
+      contactContainer.current,
+      buttons
+    )?.play();
 
-      if (showSliderTl.current && contactSlideTl.current) {
-        showSliderTl.current
-          .play()
-          .then(() => contactSlideTl.current && contactSlideTl.current.play());
-      }
-    },
-    { scope: ref }
-  );
+    showServiceTL.current = showServices(serviceContainer.current, buttons);
+  });
 
-  //Need to refactor refs to each container, be easier than breaking into nested children.
-  // Also refactor animations to show/hide each container once animation complete.
   useLayoutEffect(() => {
-    if (!serviceSlideTl.current || !contactSlideTl.current) return;
+    if (!buttonsContainer.current) return;
 
     const switchSlide = async () => {
       if (activeSlide === 0) {
         console.log("contact");
-        await contactSlideTl.current!.reverse(0);
+        await showContactTL.current?.reverse();
+        await showServiceTL.current?.play();
+        activeSlide = 1;
       } else {
         console.log("service");
-        serviceSlideTl.current!.reverse();
+        await showServiceTL.current?.reverse();
+        await showContactTL.current?.play();
+        activeSlide = 0;
       }
-      // activeSlide === 0 ? contactSlideTl.current.reverse(0) : serviceSlideTl.current.reverse(0);
     };
 
-    setInterval(switchSlide, 10 * 1000);
+    setInterval(switchSlide, 15 * 1000);
   }, []);
 
   return (
@@ -77,16 +74,25 @@ const AboveFold: React.FC = (): ReactElement => {
         </h2>
       </div>
 
-      <div className="hero-slider" ref={ref}>
+      <div className="hero-slider">
         <div className="hero-slider__img-container">
-          <div className="hero-slider__img hero-slider__contact">
+          <div
+            className="hero-slider__img hero-slider__contact"
+            ref={contactContainer}
+          >
             <h2>Free Estimates</h2>
             <p>Contact us today for a free no obligation quote</p>
             <button>Contact</button>
-            <img src="./images/hero_contact_background.webp" alt="hero contact background" />
+            <img
+              src="./images/hero_contact_background.webp"
+              alt="hero contact background"
+            />
           </div>
 
-          <div className="hero-slider__img hero-slider__services">
+          <div
+            className="hero-slider__img hero-slider__services"
+            ref={serviceContainer}
+          >
             <h2>Services</h2>
             <img
               src="https://st.depositphotos.com/1031174/2234/i/450/depositphotos_22345609-stock-photo-work-tools.jpg"
@@ -130,7 +136,7 @@ const AboveFold: React.FC = (): ReactElement => {
             </div>
           </div>
         </div>
-        <div className="hero-slider__buttons">
+        <div className="hero-slider__buttons" ref={buttonsContainer}>
           <div className="hero-slider__button"></div>
           <div className="hero-slider__button"></div>
         </div>
--- src/components/utils/animations.ts
+++ src/components/utils/animations.ts
@@ -1,11 +1,24 @@
 import gsap from "gsap";
-import { title } from "process";
 
-export const showHeroSlider = (contact: Element, buttons: Element[]): GSAPTimeline | null => {
-  if (!contact) return null;
+type ShowContact = (
+  contact: Element,
+  buttons: Element[]
+) => GSAPTimeline | undefined;
 
-  const tl = gsap
-    .timeline({ paused: true, delay: 0.5 })
+export const showContact: ShowContact = (contact, buttons) => {
+  if (!contact) return;
+
+  return gsap
+    .timeline({
+      delay: 0.5,
+      onStart: function () {
+        gsap.set(contact, { display: "flex" });
+      },
+      onReverseComplete: function () {
+        gsap.set(contact, { display: "none" });
+      },
+    })
+
     .fromTo(
       contact,
       {
@@ -17,23 +30,6 @@ export const showHeroSlider = (contact: Element, buttons: Element[]): GSAPTimeli
         scale: 1,
       }
     )
-    .fromTo(
-      buttons,
-      { autoAlpha: 0, scale: 0, stagger: 0.2 },
-      {
-        autoAlpha: 1,
-        scale: 1,
-        stagger: 0.2,
-      }
-    );
-  return tl;
-};
-
-export const showContactSlide = (contact: Element, paused: boolean = true): GSAPTimeline | null => {
-  if (!contact) return null;
-
-  return gsap
-    .timeline({ paused })
     .fromTo(
       contact.children[0],
       {
@@ -65,33 +61,44 @@ export const showContactSlide = (contact: Element, paused: boolean = true): GSAP
       {
         y: 0,
         autoAlpha: 1,
-        onComplete: function () {
-          gsap.set(this.targets(), { clearProps: "all" });
-        },
-      }
-    );
-};
-
-export const showServiceSlide = (service: Element): GSAPTimeline | null => {
-  if (!service) return null;
-
-  const container = service.children[2];
-  const services = Array.from(container.children);
-  const titles = services.map((service) => service.children[1]);
-
-  const tl = gsap
-    .timeline()
-    .fromTo(
-      services,
-      { autoAlpha: 0, scale: 0, skewX: -100 },
-      {
-        autoAlpha: 1,
-        scale: 1.2,
-        stagger: 0.1,
-        skewX: 0,
       }
     )
-    .to(services, { scale: 1 })
-    .fromTo(titles, { y: 10, autoAlpha: 0 }, { y: 0, autoAlpha: 1, stagger: 0.1 });
-  return tl;
+    .fromTo(
+      buttons,
+      { autoAlpha: 0, scale: 0, stagger: 0.2 },
+      {
+        autoAlpha: 1,
+        scale: 1,
+        stagger: 0.2,
+      }
+    )
+    .to(buttons[0], { backgroundColor: "#000" });
+};
+
+type ShowServices = (
+  services: Element,
+  buttons: Element[]
+) => GSAPTimeline | undefined;
+
+export const showServices: ShowServices = (services, buttons) => {
+  if (!services || !buttons) return;
+  const serviceIcons = services.children[2];
+  return gsap
+    .timeline({
+      paused: true,
+      delay: 0.5,
+      onStart: function () {
+        gsap.set(services, { display: "flex" });
+      },
+      onReverseComplete: function () {
+        gsap.set(services, { display: "none" });
+      },
+    })
+    .fromTo(services, { autoAlpha: 0 }, { autoAlpha: 1 })
+    .fromTo(
+      serviceIcons.children,
+      { scale: 0, autoAlpha: 0, stagger: 0.1 },
+      { scale: 1.2, autoAlpha: 0.5, stagger: 0.1 }
+    )
+    .to(serviceIcons.children, { scale: 1, autoAlpha: 1 });
 };
