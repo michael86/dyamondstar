48cac30f1184e0b83a8af300853a3a56a877328a
michael86 <michael8t6@gmail.com>
2024-01-06 15:43:24 +0000

    begun creating service containers

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,55 @@
 import { ReactElement } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHammer } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBathtub,
+  faCaravan,
+  faGlassWater,
+  faHammer,
+  faKitchenSet,
+  faPaintRoller,
+  faSailboat,
+} from "@fortawesome/free-solid-svg-icons";
 import "./styles/services.css";
+
+const services = [
+  {
+    img: "cxci0",
+    icon: faHammer,
+    title: "carpentry",
+    linkTo: "https://facebook.com",
+  },
+  {
+    title: "gardening",
+    img: "",
+    icon: faGlassWater,
+    linkTo: "https://facebook.com",
+  },
+  {
+    title: "bathrooms",
+    img: "",
+    icon: faBathtub,
+    linkTo: "https://facebook.com",
+  },
+  {
+    title: "kitchens",
+    img: "",
+    icon: faKitchenSet,
+    linkTo: "https://facebook.com",
+  },
+  { title: "boats", img: "", icon: faSailboat, linkTo: "https://facebook.com" },
+  {
+    title: "caravans",
+    img: "",
+    icon: faCaravan,
+    linkTo: "https://facebook.com",
+  },
+  {
+    title: "decorating",
+    img: "",
+    icon: faPaintRoller,
+    linkTo: "https://facebook.com",
+  },
+];
 
 const Services: React.FC = (): ReactElement => {
   return (
@@ -10,6 +58,24 @@ const Services: React.FC = (): ReactElement => {
         <FontAwesomeIcon className="services-title--icon" icon={faHammer} />
         <h2>Our Services</h2>
         <p className="services-title--sub">What We Can Do</p>
+      </div>
+      <p className="services--sub-title">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        molestias ipsam quia, consequatur expedita voluptates sit esse excepturi
+        porro reprehenderit.
+      </p>
+
+      <div className="services--service-container">
+        {services.map((service) => {
+          return (
+            <>
+              <div className="services--service" key={service.title}>
+                <FontAwesomeIcon icon={service.icon} />
+                <h3>{service.title}</h3>
+              </div>
+            </>
+          );
+        })}
       </div>
     </section>
   );
