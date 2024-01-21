932b18cc47947136d6f58f0f5201bd8e679452d2
michael86 <michael8t6@gmail.com>
2024-01-21 11:52:33 +0000

    servicce cards

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  IconDefinition,
   faBathtub,
   faCaravan,
   faGlassWater,
@@ -11,41 +12,61 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import "./styles/services.css";
 
-const services = [
+type Services = {
+  title: string;
+  img: string;
+  alt: string;
+  icon: IconDefinition;
+  linkTo: string;
+}[];
+
+const services: Services = [
   {
-    img: "cxci0",
-    icon: faHammer,
     title: "carpentry",
+    img: "./images/service_carpentry.jpg",
+    alt: "carpentry service",
+    icon: faHammer,
     linkTo: "https://facebook.com",
   },
   {
     title: "gardening",
-    img: "",
+    img: "./images/service_gardening.jpg",
+    alt: "gardening service",
     icon: faGlassWater,
     linkTo: "https://facebook.com",
   },
   {
     title: "bathrooms",
-    img: "",
+    img: "./images/service_bathroom.jpg",
+    alt: "bathroom service",
     icon: faBathtub,
     linkTo: "https://facebook.com",
   },
   {
     title: "kitchens",
-    img: "",
+    img: "./images/service_kitchen.jpg",
+    alt: "kitchen service",
     icon: faKitchenSet,
     linkTo: "https://facebook.com",
   },
-  { title: "boats", img: "", icon: faSailboat, linkTo: "https://facebook.com" },
+  {
+    title: "boats",
+    img: "./images/service_boats.jpg",
+    alt: "boats service",
+    icon: faSailboat,
+    linkTo: "https://facebook.com",
+  },
   {
     title: "caravans",
-    img: "",
+    img: "./images/service_caravan.jpg",
+    alt: "caravans service",
     icon: faCaravan,
     linkTo: "https://facebook.com",
   },
   {
     title: "decorating",
-    img: "",
+    img: "./images/service_painting.webp",
+    alt: "decorating service",
     icon: faPaintRoller,
     linkTo: "https://facebook.com",
   },
@@ -69,9 +90,17 @@ const Services: React.FC = (): ReactElement => {
         {services.map((service) => {
           return (
             <>
-              <div className="services--service" key={service.title}>
-                <FontAwesomeIcon icon={service.icon} />
-                <h3>{service.title}</h3>
+              <div
+                className="services--service"
+                key={service.title}
+                style={{
+                  backgroundImage: `linear-gradient(to bottom, rgba(245, 246, 252, 0.1), rgba(0,0,0, 0.73)), url(${service.img})`,
+                }}
+              >
+                <div>
+                  <FontAwesomeIcon className="icon" icon={service.icon} />
+                  <h3>{service.title}</h3>
+                </div>
               </div>
             </>
           );
